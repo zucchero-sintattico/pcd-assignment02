a773d663787a2157fe099798eefe24bc6b4d4376
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-05-12 13:16:27 +0200

    import rxJava

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation("io.vertx:vertx-web:4.4.1")
     implementation("io.vertx:vertx-web-client:4.4.1")
     runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.76.Final:osx-aarch_64")
+    /* module 2.3 - reactive programming - RxJava */
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.4")
 }
 
 tasks.getByName<Test>("test") {
--- src/main/java/assignment02/reactive/Main.java
+++ src/main/java/assignment02/reactive/Main.java
@@ -1,7 +1,8 @@
 package assignment02.reactive;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
