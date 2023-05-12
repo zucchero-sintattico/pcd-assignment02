599f180037f9c96b9723e349d102affd76525028
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-05-12 16:18:53 +0200

    fix main in reactive step

--- src/main/java/assignment02/reactive/Main.java
+++ src/main/java/assignment02/reactive/Main.java
@@ -1,9 +1,11 @@
 package assignment02.reactive;
+import assignment02.lib.report.ReportConfiguration;
+
 import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-       ReactiveSourceAnalyzer reactiveSourceAnalyzer = new ReactiveSourceAnalyzer();
+       ReactiveSourceAnalyzer reactiveSourceAnalyzer = new ReactiveSourceAnalyzer(new ReportConfiguration(3, 10, 20));
        reactiveSourceAnalyzer.analyzeSources(Path.of("src"));
     }
 }
