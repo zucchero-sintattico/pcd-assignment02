afd619d5e970f5d1046b605666b52f9045ab8599
nicola-ferrarese <f.nicola98@gmail.com>
2023-05-19 17:03:14 +0200

    * [init] aggiunta classe Test

---
+++ src/main/java/assignment02/Test.java
@@ -0,0 +1,62 @@
+package assignment02;
+import assignment02.event.EventBasedSourceAnalyser;
+import assignment02.executor.TaskBasedSourceAnalyzer;
+import assignment02.lib.report.Report;
+import assignment02.lib.report.ReportConfiguration;
+
+import java.io.PrintStream;
+import java.nio.file.Path;
+import java.util.concurrent.Future;
+
+public class Test {
+    public static void main(String[] args) {
+        Test test = new Test();
+        long time = test.RunEventBasedSourceAnalyser();
+        System.out.println("EventBasedSourceAnalyser: " + time + "ms");
+        time = test.RunExecutorBasedSourceAnalyzer();
+        System.out.println("ExecutorBasedSourceAnalyzer: " + time + "ms");
+    }
+    long RunEventBasedSourceAnalyser(){
+        final ReportConfiguration configuration = new ReportConfiguration(3, 10, 20);
+        final SourceAnalyzer sourceAnalyser = new EventBasedSourceAnalyser(configuration);
+        final Future<Report> report = sourceAnalyser.getReport(Path.of("src"));
+        final long tic = System.currentTimeMillis();
+        try {
+            report.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        final long toc = System.currentTimeMillis();
+        return toc - tic;
+    }
+
+    long RunExecutorBasedSourceAnalyzer(){
+        final ReportConfiguration configuration = new ReportConfiguration(3, 10, 20);
+        final SourceAnalyzer sourceAnalyser = new TaskBasedSourceAnalyzer(configuration);
+        final Future<Report> report = sourceAnalyser.getReport(Path.of("src"));
+        long tic = System.currentTimeMillis();
+        PrintStream old = System.out;
+
+        /////////////////////////////////////////////////////
+        System.setOut(new PrintStream(new OutputStream() {
+            @Override
+            public void write(int b) {
+                //DO NOTHING
+            }
+        }));
+        ///////////////////////////////////////////////////
+
+        try {
+
+            report.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        long toc = System.currentTimeMillis();
+        return toc - tic;
+    }
+
+
+
+}
+
